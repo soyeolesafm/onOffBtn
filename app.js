8f444a807a601651ae5f1af0850fb50bc9e65f32
soyeolesafm <soyelesafm@hotmail.com>
2021-10-31 14:05:35 -0500

    i made sme changes but still messy

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const checkbox = document.getElementById(`checkbox`);
+const footer = document.getElementsByClassName(`.footer`)
+const fabGH = document.getElementsByClassName(`.fa-github`)
+const icono = document.getElementById(icon)
 
+console.log(icono);
 checkbox.addEventListener (`change`, () =>  {
     document.body.classList.toggle(`dark`)
+    footer.classList.toggle(`invert`)
+    fabGH.classList.toggle(`ghdark`)
+    // if (checkbox = checked) {
+    //     console.log('i am checked');
+    // } else {
+    //     console.log('not checked');
+    // }
 })
+
